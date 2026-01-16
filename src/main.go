e47a210f9d3f844fed76afe323be490772736867
cyokozai <yuu4suke27@outlook.jp>
2026-01-16 13:05:52 +0900

    Add initial game logic and configuration files for breakout game

---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/sago35/koebiten"
+	"github.com/sago35/koebiten/games/blocks/blocks"
+	"github.com/sago35/koebiten/hardware"
+
+	"github.com/cyokozai/breakout/breakout"
+)
+
+func main() {
+	koebiten.SetHardware(hardware.Device)
+	// koebiten.SetRotation(koebiten.Rotation90)
+	// koebiten.SetWindowSize(64, 128)
+	// koebiten.SetWindowTitle("Breakout in Go")
+
+	game := breakout.NewGame()
+
+	if err := koebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
