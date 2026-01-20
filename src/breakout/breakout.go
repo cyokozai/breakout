0c4ebae238f5eb732c6b66a243d726fcfe9ca430
cyokozai <yuu4suke27@outlook.jp>
2026-01-21 05:52:59 +0900

    ゲームの初期化と状態管理を改善し、画面サイズを調整

--- src/breakout/breakout.go
+++ src/breakout/breakout.go
@@ -1,17 +1,16 @@
 package breakout
 
 import (
-	"time"
-
  	"github.com/sago35/koebiten"
+	"tinygo.org/x/tinyfont/freemono"
 	"tinygo.org/x/drivers/pixel"
 )
 
 
 type Game struct {
-	ticker *time.Ticker
-	timer int
-	score int
+	// ticker *time.Ticker
+	// timer int
+	// score int
 	ball_x, ball_y int
 	ball_vx, ball_vy int
 	paddle_x, paddle_y int
@@ -20,29 +19,66 @@ type Game struct {
 }
 
 var (
-	screenWidth  = 64
-	screenHeight = 128
+	screenWidth  = 128
+	screenHeight = 64
+	font				 = &freemono.Bold9pt7b
+	area_X       = 4
+	area_Y       = 1
+	areaWidth    = screenWidth - area_X * 2
+	areaHeight   = screenHeight - area_Y * 2
 	paddleWidth  = 2
 	paddleHeight = 16
+	initPaddle_X = 8
+	initPaddle_Y = (areaHeight - paddleHeight) / 2
+	initBall_X   = areaWidth / 2
+	initBall_Y   = areaHeight / 2
 	ballRadius   = 2
 	ballVelocity = 2
+	initBlocks_X = 16
+	initBlocks_Y = 8
 	blockWidth   = 8
 	blockHeight  = 12
 	white = pixel.NewMonochrome(0xFF, 0xFF, 0xFF)
 	black = pixel.NewMonochrome(0x00, 0x00, 0x00)
 )
 
+// var (
+// 	textTimer_X int16 = 32
+// 	textTimer_Y int16 = 1
+// 	initTimer    			= 300
+// 	textScore_X int16 = 1
+// 	textScore_Y int16 = 1
+// 	initScore    			= 0
+// 	maxScore     			= 999
+// )
+
+func (g *Game) Reset() {
+	g.ball_x   = initBall_X
+	g.ball_y   = initBall_Y
+	g.ball_vx  = ballVelocity
+	g.ball_vy  = ballVelocity
+	g.paddle_x = initPaddle_X
+	g.paddle_y = initPaddle_Y
+	// g.timer    = initTimer
+	// g.score    = initScore
+	g.state    = "playing"
+}
+
+func (g *Game) IsGameOver() bool {
+	return g.state == "gameover"
+}
+
 func (g *Game) PaddleControl() {
 	if koebiten.IsKeyPressed(koebiten.KeyArrowUp) {
-		if g.paddle_y <= 0 {
-			g.paddle_y = 0
+		if g.paddle_y <= area_Y {
+			g.paddle_y = area_Y
 		} else {
 			g.paddle_y--
 		}
 	}
 	if koebiten.IsKeyPressed(koebiten.KeyArrowDown) {
-		if g.paddle_y >= screenWidth - paddleHeight {
-			g.paddle_y = screenWidth - paddleHeight
+		if g.paddle_y + paddleHeight >= areaHeight {
+			g.paddle_y = areaHeight - paddleHeight
 		} else {
 			g.paddle_y++
 		}
@@ -53,17 +89,17 @@ func (g *Game) BallMove() {
 	g.ball_x += g.ball_vx
 	g.ball_y += g.ball_vy
 
-	if g.ball_y <= 0 || g.ball_y >= screenWidth {
+	if g.ball_y <= area_Y || g.ball_y >= areaHeight {
 		g.ball_vy = -g.ball_vy
 	}
-	if g.ball_x >= screenHeight {
+	if g.ball_x >= areaWidth {
 		g.ball_vx = -g.ball_vx
 	}
 	
 	if g.ball_y + ballRadius >= g.paddle_y && g.ball_y - ballRadius <= g.paddle_y + paddleHeight {
-		if g.ball_x + ballRadius > g.paddle_x && g.ball_x - ballRadius < g.paddle_x + paddleWidth {
+		if g.ball_x + ballRadius > g.paddle_x + paddleWidth && g.ball_x - ballRadius < g.paddle_x + paddleWidth {
 			if g.ball_y > 0 {
-				g.ball_vx *= -1
+				g.ball_vx = -g.ball_vx
 				g.ball_x = g.paddle_x + paddleWidth + ballRadius
 			}
 		}
@@ -92,25 +128,26 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *koebiten.Image) {
-	koebiten.DrawFilledRect(screen, g.paddle_x, g.paddle_y ,paddleWidth, paddleHeight, white)
+	koebiten.DrawRect(screen, area_X, area_Y, areaWidth, areaHeight, white)
+	koebiten.DrawFilledRect(screen, g.paddle_x, g.paddle_y, paddleWidth, paddleHeight, white)
 	koebiten.DrawFilledCircle(screen, g.ball_x, g.ball_y, ballRadius, white)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return screenHeight, screenWidth
+	return screenWidth, screenHeight
 }
 
 func NewGame() *Game {
 	game := &Game{
-		ticker: time.NewTicker(time.Second),
-		timer: 300,
-		score: 0,
-		ball_x: screenHeight / 2,
-		ball_y: screenWidth / 2,
+		// ticker: time.NewTicker(time.Second),
+		// timer: initTimer,
+		// score: initScore,
+		ball_x: initBall_X,
+		ball_y: initBall_Y,
 		ball_vx: ballVelocity,
 		ball_vy: ballVelocity,
-		paddle_x: 8,
-		paddle_y: (screenWidth - paddleHeight) / 2,
+		paddle_x: initPaddle_X,
+		paddle_y: initPaddle_Y,
 		blocks: func() [][2]int {
 			b := make([][2]int, 0)
 			for i := 0; i < 5; i++ {
